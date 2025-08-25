8bdcaa990fa4a60881b40310c681e1b7de5c3a00
Rayen Malouche <rayenmalouche27@gmail.com>
2025-08-25 11:20:39 +0100

    first commit

---
+++ src/test/java/com/mcp/RayenMalouche/performance/testing/server/PerformanceTestingServer/PerformanceTestingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.RayenMalouche.performance.testing.server.PerformanceTestingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTestingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
